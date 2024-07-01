3400e8b33fcd0c52fb86953b4eca98a1c2da11c0
dyi919 <ehddjs13579@gmail.com>
2024-07-02 03:05:44 +0900

    Add boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>TO-DO</h1>
+      <button>Add To-do</button>
+      <ul>
+        <li>
+          <input type='checkbox' />
+          <span>todo 1</span>
+          <button>Delete</button>
+        </li>
+        <li>
+          <input type='checkbox' />
+          <span>todo 2</span>
+          <button>Delete</button>
+        </li>
+        <li>
+          <input type='checkbox' />
+          <span>todo 3</span>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
