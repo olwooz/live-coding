afede9b78df32c3298ebf5bfa19bce18c8581e4d
dyi919 <ehddjs13579@gmail.com>
2024-07-02 02:58:41 +0900

    Add boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <h3>Time: </h3>
+      <br />
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
+    </div>
+  );
 }
 
 export default App;
